562c8e36cc831b561b06beaf7aec294324b20f40
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2018-09-14 23:04:26 +0200

    Initial

---
+++ src/main/scala/com/pako/png/PngParser.scala
@@ -0,0 +1,5 @@
+package com.pako.png
+
+class PngParser {
+
+}
---
+++ src/test/scala/com/pako/png/PngParserSpec.scala
@@ -0,0 +1,10 @@
+package com.pako.png
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class PngParserSpec extends FlatSpec with Matchers {
+  "PngParser" should "read file" in {
+    true shouldEqual true
+  }
+
+}
